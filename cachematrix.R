19c30d259d74e0ec55189cab573488b4f1e8da70
creator421 <hohoteariki.moanaroa@riotinto.com>
2015-02-21 23:50:58 +1100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -45,3 +45,18 @@ cacheSolve <- function(x, ...) {
 }
 
 ## Test Data
+## m <- makeCacheMatrix()
+## m$get(matrix(c(0,2,2,0), 2,2))
+## m$get
+##          [,1] [,2]
+##   [1,]    0    2
+##   [2,]    2    0
+## cacheSolve(m)
+##         [,1] [,2]
+##   [1,]    0.0    0.5
+##   [2,]    0.5    0.0
+## cacheSolve(m)
+## getting cached data
+##         [,1] [,2]
+##   [1,]    0.0    0.5
+##   [2,]    0.5    0.0
